6408bf3d076e437abbbb6703991c8874dcff9fbe
smirfolio <ben.sghaier.samir@gmail.com>
2015-02-06 14:12:58 -0500

    Maelstrom theme MLSTRM-194 Contacts/Investigators team in about page Principal Investigator group view Investigator group view

--- mica/maelstrom_bootstrap/templates/views-view-unformatted--investigators.tpl.php
+++ mica/maelstrom_bootstrap/templates/views-view-unformatted--investigators.tpl.php
@@ -2,7 +2,7 @@
 <div class="team headline"><h2><?php print t('Principal Investigators')?> </h2></div>
 <div class="row team margin-bottom-20">
   <?php foreach ($rows as $key => $row): ?>
-  <div class="col-sm-12 col-md-3 margin-top-25 pull-right">
+  <div class="col-sm-12 col-md-3 margin-top-25">
       <?php print render($row); ?>
   </div>
   <?php endforeach; ?>
